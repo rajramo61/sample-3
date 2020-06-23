69937800692c79157b8969f145fe07775e5b830c
Rajesh Dwivedi <Rajesh-Kumar.Dwivedi@kp.org>
2020-06-22 21:38:07 -0700

    Initial commit

---
+++ src/main/java/org/example3/Sample3.java
@@ -0,0 +1,11 @@
+package org.example3;
+
+/**
+ * @author Rajesh Dwivedi
+ * Date: 6/22/20
+ */
+public class Sample3 {
+    public void sayHello() {
+        System.out.println("Hello from example3 Sample3.");
+    }
+}
